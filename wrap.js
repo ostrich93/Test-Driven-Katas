4d0eaa78f059ca7fed824647c548666fe811fe79
Ezugworie I.e <ezugworie.ikechukwu@live.com>
2018-08-13 10:43:33 -0500

    Working out the kinks in wrap

--- wrap.js
+++ wrap.js
@@ -1,26 +1,26 @@
 function wrap(line, maxLen) {
+  let output = [];
 
-    let output = [];
+  let wordArr = line.split(' ');
+  console.log('This is the word array =>', wordArr);
 
-    let wordArr = line.split(' ');
+  // wordArr.every(entry => !entry)
+  if (!line || typeof line !== 'string' || typeof maxLen !== 'number') {
+    return '';
+  }
 
-    // wordArr.every(entry => !entry)
-    if (!line || typeof line !== 'string' || typeof maxLen !== 'number'){
-        return '';
+  let currentLine = '';
+
+  for (var a = 0; a < wordArr.length; a++) {
+    let currentWord = wordArr[a];
+    if (currentLine.length + currentWord.length > maxLen) {
+      output.push(currentLine);
+      currentLine = currentWord;
+    } else {
+      currentLine += currentWord + ' ';
     }
-
-    let currentLine = '';
-    for (var a=0; a < wordArr.length; a++){
-        let currentWord = wordArr[a];
-        if (currentLine.length + currentWord.length > maxLen) {
-            output.push(currentLine);
-            currentLine = currentWord;
-        }
-        else {
-            currentLine += currentWord;
-        }
-    }
-    return output.join('\n');
+  }
+  return output.join('\n');
 }
 
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -1,9 +1,15 @@
-const {expect} = require('chai');
+const { expect } = require('chai');
 const wrap = require('./wrap');
 
-describe('wrap()', function(){
-    it('returns an empty string if empty string was provided.', function(){
-        expect(wrap("", 10)).to.equal("");
-    });
-    
+describe('wrap()', function() {
+  it('returns an empty string if empty string was provided.', function() {
+    expect(wrap('', 10)).to.equal('');
+  });
+  it('return expected output given by the prompt. ', function() {
+    const phrase =
+      'Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.';
+    const expectedOutput =
+      'Lorem ipsum dolor\n sit eu amet, elit na\n magna sem amet nulla\n vel purus ac ligula.';
+    expect(wrap(phrase, 20)).to.equal(expectedOutput);
+  });
 });
