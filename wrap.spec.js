f4dedd0bbfba7e8340b026c1855d59872ecebe6b
Alex Morson <amorson@conncoll.edu>
2018-08-13 09:48:17 -0500

    first commit

---
+++ wrap.spec.js
@@ -0,0 +1,5 @@
+describe('wrap()', function(){
+    it('returns a string with line breaks inserted in correct places.', function(){
+        const j = wrap("Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.", 20);
+    });
+});
